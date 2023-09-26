26c10338e3902914cc8bbaea9e597003ab607dcc
Paul <paul@theapplegates.com>
2023-09-25 21:47:56 -0400

    first commit

---
+++ src/site.config.ts
@@ -0,0 +1,39 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+	author: "Paul Applegate",
+	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+	title: "Living Life One Cactus At A Time",
+	// Meta property used as the default description meta property
+	description: "My views on everything, I might not agree with you, but I support your right to say it.",
+	// HTML lang property, found in src/layouts/Base.astro L:18
+	lang: "en-US",
+	// Meta property, found in src/components/BaseHead.astro L:42
+	ogLocale: "en_US",
+	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+	date: {
+		locale: "en-GB",
+		options: {
+			day: "numeric",
+			month: "short",
+			year: "numeric",
+		},
+	},
+};
+
+// Used to generate links in both the Header & Footer.
+export const menuLinks: Array<{ title: string; path: string }> = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "About",
+		path: "/about/",
+	},
+	{
+		title: "Blog",
+		path: "/posts/",
+	},
+];
